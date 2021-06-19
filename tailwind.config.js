8886712d2dfc48e8337ba4ac650fca7c6bc31d7e
Stinggyray <stinggyray@gmail.com>
2021-06-18 22:42:36 -0500

    reject modernity, return to true dark

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,13 @@
+const colors = require('tailwindcss/colors')
+
+let grayColors = colors.gray;
+
+//grayColors[700] = "#2c2f33"
+
+// Reject modernity, return to AMOLED
+grayColors[700] = "#111111"
+grayColors[800] = "#000000"
+
 module.exports = {
   purge: {
     content: [
@@ -7,7 +17,12 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: grayColors, //colors.gray //colors.trueGray
+        yellow: colors.amber
+      },
+    },
   },
   variants: {
     extend: {},
