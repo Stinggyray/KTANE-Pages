d0264426b340992636d32239c9a5b85b451cbdd5
Stinggyray <stinggyray@gmail.com>
2021-06-20 16:18:30 -0500

    update everything

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,8 @@ let grayColors = colors.gray;
 //grayColors[700] = "#2c2f33"
 
 // Reject modernity, return to AMOLED
-grayColors[700] = "#111111"
-grayColors[800] = "#000000"
+//grayColors[700] = "#111111"
+//grayColors[800] = "#000000"
 
 module.exports = {
   purge: {
